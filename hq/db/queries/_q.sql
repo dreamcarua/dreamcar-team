4b0ef961bf0a5b6085790f7b01022af4b527afda
Vadym Gryshyn <vg@abrisart.com>
2026-08-17 18:56:14 +0200

    q [skip ci]

--- hq/db/queries/_q.sql
+++ hq/db/queries/_q.sql
@@ -1,7 +1,9 @@
-with agg as (
-  select project, (paid_at at time zone 'Europe/Kyiv')::date d, count(*) c, sum(amount) s
-  from dashboard_deals
-  where status='pay' and project in ('IPHONE 17 PRO MAX','3 IPHONE')
-  group by 1,2
-)
-select json_agg(json_build_object('p',project,'d',d,'c',c,'s',s) order by project, d) as r from agg;
+select json_build_object(
+  'refresh_jobs', (select json_agg(json_build_object('jobid',jobid,'sched',schedule,'cmd',left(command,200)))
+      from cron.job where command ilike '%mv_dashboard_projects_stats%' or command ilike '%refresh%projects%'),
+  'rpc_src', (select left(pg_get_functiondef(p.oid), 700) from pg_proc p join pg_namespace n on n.oid=p.pronamespace
+      where p.proname='dashboard_projects_with_stats' and n.nspname='public' limit 1),
+  'mv_size', (select pg_size_pretty(pg_total_relation_size('mv_dashboard_projects_stats'))),
+  'deals_rows', (select count(*) from dashboard_deals),
+  'refreshed_at', (select max(refreshed_at) from mv_dashboard_projects_stats)
+) as r;
