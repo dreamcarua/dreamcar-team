4335fa5146a9486304a38c7e45ca96e4b403b627
Vadym Gryshyn <vg@abrisart.com>
2026-08-17 18:53:37 +0200

    q [skip ci]

--- hq/db/queries/_q.sql
+++ hq/db/queries/_q.sql
@@ -1,13 +1,12 @@
-with x as (
-  select p.id, p.code, p.name, p.date_start, p.date_end, unnest(p.deal_project_values) as val
-  from dashboard_projects p
-)
 select json_build_object(
-  'overlaps', (select json_agg(o) from (
-      select val, count(*) c, json_agg(json_build_object('code',code,'name',name,'start',date_start) order by date_start) projects
-      from x group by val having count(*)>1 order by c desc) o),
-  'cols_projects', (select json_agg(column_name order by ordinal_position)
-      from information_schema.columns where table_name='dashboard_projects'),
-  'all_projects', (select json_agg(json_build_object('code',code,'vals',deal_project_values,'s',date_start,'e',date_end) order by date_start desc)
-      from dashboard_projects)
+  'ipm_by_day', (select json_agg(x order by (x->>'d')) from (
+      select json_build_object('d', (paid_at at time zone 'Europe/Kyiv')::date, 'c', count(*), 's', sum(amount)) x
+      from dashboard_deals
+      where project = 'IPHONE 17 PRO MAX' and status='pay'
+      group by 1) t),
+  'three_by_day', (select json_agg(x order by (x->>'d')) from (
+      select json_build_object('d', (paid_at at time zone 'Europe/Kyiv')::date, 'c', count(*), 's', sum(amount)) x
+      from dashboard_deals
+      where project = '3 IPHONE' and status='pay'
+      group by 1) t2)
 ) as r;
