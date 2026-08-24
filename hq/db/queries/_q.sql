fe6fbd1fb48f3d779f97f792af65ae9307d03244
Vadym Gryshyn <vg@abrisart.com>
2026-08-24 15:14:26 +0200

    q [skip ci]

--- hq/db/queries/_q.sql
+++ hq/db/queries/_q.sql
@@ -1,2 +1,4 @@
-select json_agg(json_build_object('id',jobid,'sched',schedule,'active',active,'cmd',left(command,110)) order by jobid) as r
-from cron.job where active;
+select json_agg(json_build_object('id',jobid,'sched',schedule,'fn',
+   substring(command from 'functions/v1/([a-z0-9-]+)')) order by jobid) as r
+from cron.job
+where active and command ~* '(smm|watchdog|kasa-stale|ig-comment|autosvit|meta|stats)';
