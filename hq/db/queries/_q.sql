d1708c1da92e91a01b53215fd66c7e438908b3f6
Vadym Gryshyn <vg@abrisart.com>
2026-08-24 15:18:48 +0200

    CHANGELOG: нічний throttle + арифметика приватизації репо

--- hq/db/queries/_q.sql
+++
@@ -1,4 +0,0 @@
-select json_agg(json_build_object('id',jobid,'sched',schedule,'fn',
-   substring(command from 'functions/v1/([a-z0-9-]+)')) order by jobid) as r
-from cron.job
-where active and command ~* '(smm|watchdog|kasa-stale|ig-comment|autosvit|meta|stats)';
