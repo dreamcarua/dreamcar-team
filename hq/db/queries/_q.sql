0c8e02d3f3bc9e5a281c0dee23b7e027b88be425
Vadym Gryshyn <vg@abrisart.com>
2026-08-27 17:36:30 +0200

    q [skip ci]

--- hq/db/queries/_q.sql
+++ hq/db/queries/_q.sql
@@ -1,2 +1,11 @@
-select json_agg(json_build_object('status',status_code,'body',convert_from(content,'UTF8')) order by id desc) as r
-from (select * from net._http_response order by id desc limit 2) t;
+select json_build_object(
+  'watchdog_test', (select json_build_object('status',status_code,'body',left(content,400))
+     from net._http_response order by id desc limit 1),
+  'pg_net_last_hour', (select count(*) from net._http_response where created > now() - interval '1 hour'),
+  'cron_jobs_active', (select count(*) from cron.job where active),
+  'cron_http_per_day', (select sum(case
+       when schedule ~ '^\*/([0-9]+)' then 1440 / nullif((regexp_match(schedule,'^\*/([0-9]+)'))[1]::int,0)
+       when schedule ~ '^[0-9,]+ \*' then array_length(string_to_array(split_part(schedule,' ',1),','),1) * 24
+       else 1 end)
+     from cron.job where active and command ilike '%http_post%')
+) as r;
