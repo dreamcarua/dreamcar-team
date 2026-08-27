7e9d0f07807f1f1a161dbf99915136d8dd95d10f
Vadym Gryshyn <vg@abrisart.com>
2026-08-27 17:33:31 +0200

    q2 [skip ci]

--- hq/db/queries/_q.sql
+++ hq/db/queries/_q.sql
@@ -1,6 +1,2 @@
--- викликаємо нову Edge-функцію з БД (там же лежить hq_cron_secret) і повертаємо відповідь
-select net.http_get(
-  url := 'https://wotghlaehnvxyeacznvv.supabase.co/functions/v1/smm-content-watchdog?dry=1',
-  headers := jsonb_build_object('x-hq-cron-secret', (select value from app_secrets where key='hq_cron_secret')),
-  timeout_milliseconds := 30000
-) as request_id;
+select json_agg(json_build_object('status',status_code,'body',convert_from(content,'UTF8')) order by id desc) as r
+from (select * from net._http_response order by id desc limit 2) t;
